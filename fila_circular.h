4c2da7ee4741e59a54c70714cfe076d5ac35dab4
isabellacgmsa <49571544+isabellacgmsa@users.noreply.github.com>
2019-06-18 15:09:08 -0300

    Update fila_circular.h

--- fila_circular.h
+++ fila_circular.h
@@ -1,11 +1,11 @@
 #ifndef _fila_circular_h
 #define _fila_circular_h
 
-void cria_fila_int();
-int enfileira_int(int y);
-int desenfileira_int();
-int fila_int_vazia();
-int fila_int_cheia();
+void cria_fila_circular();
+int enfileira_circular(int y);
+int desenfileira_circular();
+int fila_circular_vazia();
+int fila_circular_cheia();
 void print_fila();
 
 #endif
